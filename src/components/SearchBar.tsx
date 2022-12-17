62c2c0ee3a6bd9c0d9c245b60117bebb36d252be
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-12-17 15:52:36 -0500

    feat: #002 - Added SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <button className="max-w-xs">Search Bar</button>
+    </div>
+  );
+};
+
+export default SearchBar;
